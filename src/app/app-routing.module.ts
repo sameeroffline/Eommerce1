23562c84dee3548b2357205731216f4d6a3eb401
sameeroffline <sameer.cbe29@gmail.com>
2022-10-06 13:15:03 +0530

    Add existing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HelperComponent } from './helper/helper.component';
+import { OrdersComponent } from './orders/orders.component';
+ 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about',component:AboutComponent},
+  {path:'orders',component:OrdersComponent},
+  {path:'helper',component:HelperComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HelperComponent } from './helper/helper.component';
+import { LanguageComponent } from './language/language.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent,
+    OrdersComponent,
+    HelperComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatGridListModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
